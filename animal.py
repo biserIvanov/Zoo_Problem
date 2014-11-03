52a14e3d2d742f80ddba6e3dadacb044a0604072
georgi-lyubenov <georgi_lyubenov@mail.bg>
2014-11-03 20:53:48 +0200

    few changes

--- animal.py
+++ animal.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Animal():
     def __init__(self, species, age, name, gender, weight):
         self.species = species
@@ -5,13 +8,26 @@ class Animal():
         self.name = name
         self. gender = gender
         self.weight = weight
-
-    def grow(self, weight, age):
-        self.weight += weight
-        self.age += age
+        f = open("database.json", "r")
+        data = f.read()
+        jsondata = json.loads(data)
+        f.close()
+        for animal in jsondata:
+            if animal['species'] == self.species:
+                self.life_expectancy = animal['life_expectancy']
+                self.food_type = animal['food_type']
+                self.gestation_period = animal['gestation period']
+                self.newborn_weight_kilos = animal['newborn weight in kilos']
+                self.FoodWeightRatio = animal['food/weight ratio']
+                self.WeightAgeRatio = animal['weight/age ratio']
 
     def eat(self):
-        pass
+            food_to_eat = self.FoodWeightRatio * self.weight
+            self.weight += food_to_eat
+
+    def grow(self, monthly_weight):
+        while self.weight / self.age <= self.WeightAgeRatio:
+            self.weight += monthly_weight
 
     def die(self, life_expectancy):
         chance_of_dying = self.age / life_expectancy

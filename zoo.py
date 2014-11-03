f220752ccfdb9cb4798e5e436a595489e8f81c3f
biserivanov <biserivanov08@gmail.com>
2014-11-03 19:26:08 +0200

    version1

---
+++ zoo.py
@@ -0,0 +1,6 @@
+class Zoo:
+
+    def __init__(self, animalsCollection, capacity, budget):
+        self.animalsCollection = animalsCollection
+        self.capacity = capacity
+        self.budget = budget
